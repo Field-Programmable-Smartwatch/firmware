5f1aacc0220175ab895fecda449ad898b3b440e6
Alex Lanzano <lanzano.alex@gmail.com>
2020-10-25 22:25:03 -0400

    led-blink: Change GPIO pin for LED. Fix clock enable for GPIO. Reduce time constraint on LED blink.

--- led-blink/main.c
+++ led-blink/main.c
@@ -5,29 +5,29 @@
 
 void led_on()
 {
-    gpio_write(GPIOA, 14, 1);
+    gpio_write(GPIOB, 7, 1);
 }
 
 void led_off()
 {
-    gpio_write(GPIOA, 14, 0);
+    gpio_write(GPIOB, 7, 0);
 }
 
 void main()
 {
     // Enable GPIO PORT A Clock
-    RCC->AHB2RSTR |= RCC_AHB2ENR_GPIOAEN;
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
 
-    gpio_configure_pin(GPIOA, 14,
+    gpio_configure_pin(GPIOB, 7,
                        GPIO_MODE_OUTPUT,
                        GPIO_OUTPUT_TYPE_PUSH_PULL,
                        GPIO_OUTPUT_SPEED_LOW,
                        GPIO_PULL_RESISTOR_NONE);
-
     while (1) {
         led_on();
-        for (uint32_t i = 0; i < 4000000; i++);
+        for (uint32_t i = 0; i < 400000; i++);
         led_off();
-        for (uint32_t i = 0; i < 4000000; i++);
+        for (uint32_t i = 0; i < 400000; i++);
     }
 }
